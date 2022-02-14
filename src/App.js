df2d16f361755682d772c94cbd9c4bef158a3281
whilini <whi0306@naver.com>
2022-02-14 14:07:09 +0900

    react-todo list 만들기

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import Button from "./Button.js";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDOs] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDOs((currentArray) => [...currentArray, toDo]);
+    setToDo("");
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type='text'
+          placeholder='Write your to do'
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import CleanUp from "./CleanUp";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <CleanUp />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
