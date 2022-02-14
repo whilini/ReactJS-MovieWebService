0b87a0ace34fa615c7c78b8c381e8a8eb167c22b
whilini <whi0306@naver.com>
2022-02-14 18:26:28 +0900

    폴더 변경

--- src/extra/CoinTracker.js
+++ src/extra/CoinTracker.js
@@ -23,14 +23,18 @@ const CoinTracker = () => {
   return (
     <div>
       <h1>The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <select onChange={onCoinChange}>
-        {coins.map((coin, idx) => (
-          <option key={idx} value={coin.quotes.USD.price}>
-            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-          </option>
-        ))}
-      </select>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onCoinChange}>
+          {coins.map((coin, idx) => (
+            <option key={idx} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+
       <div style={{ display: "flex" }}>
         <div>USD</div>
         <input
